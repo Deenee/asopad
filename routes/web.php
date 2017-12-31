05fcb8dd6b793cc4998e0ed7e8fcce8e4517993b
David <yarteyd@gmail.com>
2017-12-31 13:34:15 +0000

    Remove closure from routes;

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::get('auth/{provider}', 'SocialAuthController@redirectToProvider');
 Route::get('auth/{provider}/callback', 'SocialAuthController@handleProviderCallback');
